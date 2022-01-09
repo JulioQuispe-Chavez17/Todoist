bb4373ee1c819443d3e8b06bb2d664b0a4758f5b
JQuispe2019 <julio.quispe@vallegrande.edu.pe>
2022-01-09 23:30:12 +0000

    add db prod

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const cors = require('cors');
 
 const todosRouter = require('./app/api/todos/router');
 const itemsRouter = require('./app/api/items/router');
@@ -9,6 +10,7 @@ const itemsRouter = require('./app/api/items/router');
 const URL = '/api/v1'
 const app = express();
 
+app.use(cors())
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
--- app/config/database.js
+++ app/config/database.js
@@ -15,9 +15,13 @@ module.exports = {
   },
   "production": {
     "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+    "password": "35b71bcef27ae66eda7b2b04848e56cdef3611ada1cd74c7719920251b640c7b",
+    "database": "mjvnirssyqefug",
+    "port": 5432,
+    "host": "ec2-44-198-214-172.compute-1.amazonaws.com",
+    "dialect": "postgres",
+    "dialectOptions": {
+      "ssl": { "rejectUnauthorized": false }
+    },
   }
 }
